dd3cc7a330487a8a0c74e8fd31b8a9243e3d2444
julissa93 <julissa.napoletano@cix.csi.cuny.edu>
2020-03-16 22:10:19 -0400

    part2 demo

--- app/myFirstComponent.js
+++ app/myFirstComponent.js
@@ -1,5 +1,8 @@
 import React, {Component} from 'react'
 
+//Two types of React components that you can create! 
+//1. Class Components - known as stateFULL components because they can manage state.
+//You can also define functions that will execute during the component's lifecycle. 
 class MyFirstComponent extends Component {
   constructor() {
     super()
@@ -21,4 +24,18 @@ class MyFirstComponent extends Component {
   }
 }
 
-export default MyFirstComponent
+//2. Functional Component - Known as stateLESS components b/c they don't manage state/data.
+//Used for creating reusable components! 
+//Here's the Counter Class component in Functional form: 
+const Counter = () => {
+    //Notice here we don't have a render() surrounding the return. 
+    return (
+        <center>
+            <h1>Counter</h1>
+            <h1>0</h1>
+        </center>
+    )
+}
+
+//export default MyFirstComponent
+export default Counter
